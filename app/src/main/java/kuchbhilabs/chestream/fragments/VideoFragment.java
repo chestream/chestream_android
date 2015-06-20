0dccddfc16c76751ebee1571aeeb00b486b05da1
naman14 <namandwivedi14@gmail.com>
2015-06-20 21:20:32 +0530

    Sliding panel layout

--- app/src/main/java/kuchbhilabs/chestream/fragments/VideoFragment.java
+++ app/src/main/java/kuchbhilabs/chestream/fragments/VideoFragment.java
@@ -1,19 +1,14 @@
 package kuchbhilabs.chestream.fragments;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import kuchbhilabs.chestream.CompressionUploadService;
 import kuchbhilabs.chestream.R;
+import kuchbhilabs.chestream.slidinguppanel.SlidingUpPanelLayout;
 
 /**
  * Created by naman on 20/06/15.
@@ -21,61 +16,21 @@ import kuchbhilabs.chestream.R;
 public class VideoFragment extends Fragment {
 
     Activity activity;
+    SlidingUpPanelLayout slidingUpPanelLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_video,null);
+        View rootView = inflater.inflate(R.layout.fragment_video, null);
 
         activity = getActivity();
 
-        Button uploadButton = (Button) rootView.findViewById(R.id.button_upload);
-        uploadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent uploadServiceInten = new Intent(activity, CompressionUploadService.class);
-                activity.startService(uploadServiceInten);
-            }
-        });
-        final GestureDetector gesture = new GestureDetector(getActivity(),
-                new GestureDetector.SimpleOnGestureListener() {
+        slidingUpPanelLayout=(SlidingUpPanelLayout) rootView.findViewById(R.id.sliding_layout);
+        slidingUpPanelLayout.setOverlayed(true);
 
-                    @Override
-                    public boolean onDown(MotionEvent e) {
-                        return true;
-                    }
+        CommentsFragment commentsFragment = new CommentsFragment();
 
-                    @Override
-                    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                                           float velocityY) {
-
-                        final int SWIPE_MAX_OFF_PATH = 250;
-                        try {
-                            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
-                                Log.d("lol","swipe up");
-
-                                CommentsFragment commentsFragment = new CommentsFragment();
-
-                                getChildFragmentManager().beginTransaction().add(R.id.comments, commentsFragment).commit();
-
-                                return false;
-                            }
-
-
-
-                        } catch (Exception e) {
-                            // nothing
-                        }
-                        return super.onFling(e1, e2, velocityX, velocityY);
-                    }
-                });
-
-        rootView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return gesture.onTouchEvent(event);
-            }
-        });
+        getChildFragmentManager().beginTransaction().add(R.id.comments, commentsFragment).commit();
 
         return rootView;
     }
