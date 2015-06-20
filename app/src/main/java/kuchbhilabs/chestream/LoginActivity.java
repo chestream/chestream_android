fffc82e049b58e103617ddef935d251d112a70ea
laavanye <laavanye.bahl@gmail.com>
2015-06-20 15:55:31 +0530

    LoginActivity
    Signed-off-by: laavanye <laavanye.bahl@gmail.com>

---
+++ app/src/main/java/kuchbhilabs/chestream/LoginActivity.java
@@ -0,0 +1,86 @@
+package kuchbhilabs.chestream;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class LoginActivity extends Activity implements SurfaceHolder.Callback {
+
+    private MediaPlayer mediaPlayer;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        surfaceView = (SurfaceView) findViewById(R.id.login_surface_view);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+
+        //TODO: Make it work to load from the assets directory
+        mediaPlayer = new MediaPlayer();
+        String UrlPath="android.resource://"+getPackageName()+"/"+R.raw.vid4;
+//        File sdcard = Environment.getExternalStorageDirectory();
+//        File videoFile = new File(sdcard, "video.mp4");
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse(UrlPath));
+            mediaPlayer.setLooping(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        try {
+            mediaPlayer.setDisplay(surfaceHolder);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mediaPlayer.release();
+    }
+
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
