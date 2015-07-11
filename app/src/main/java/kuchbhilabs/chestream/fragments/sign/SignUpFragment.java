fde110d233e9282f37f436b88dd535ead7c624d4
laavanye <laavanye.bahl@gmail.com>
2015-07-12 00:34:40 +0530

    checks for same email, username - Login
    Signed-off-by: laavanye <laavanye.bahl@gmail.com>

--- app/src/main/java/kuchbhilabs/chestream/fragments/sign/SignUpFragment.java
+++ app/src/main/java/kuchbhilabs/chestream/fragments/sign/SignUpFragment.java
@@ -7,15 +7,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import kuchbhilabs.chestream.MainActivity;
 import kuchbhilabs.chestream.R;
@@ -35,7 +42,7 @@ public class SignUpFragment extends Fragment {
     private Activity activity;
     private ProgressDialog mProgressDialog;
     Bundle b;
-
+    String username;
     public SignUpFragment() {} //Required empty constructor
 
     @Override
@@ -66,10 +73,40 @@ public class SignUpFragment extends Fragment {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usernameEditText.getText().toString();
-                Bundle bundle =b;
-                b.putString(ParseTables.Users.USERNAME,username);
-                new PushUserIntoParse().execute(bundle);
+                username = usernameEditText.getText().toString();
+                ParseQuery<ParseUser> query = ParseUser.getQuery();
+                query.whereEqualTo("email", b.getString(ParseTables.Users.EMAIL));
+                query.getFirstInBackground(new GetCallback<ParseUser>() {
+                    public void done(ParseUser object, ParseException e) {
+                        if (object == null) {
+                            Log.d("score", "The getFirst request failed.");
+                            Bundle bundle = b;
+                            b.putString(ParseTables.Users.USERNAME, username);
+                            new PushUserIntoParse().execute(bundle);
+                        } else {
+
+                            ParseUser.logInInBackground(object.getUsername(), "ChestreamPasswordYo", new LogInCallback() {
+                                public void done(ParseUser user, ParseException e) {
+                                    if (user != null) {
+                                        // Hooray! The user is logged in.
+                                        mProgressDialog.dismiss();
+                                        Intent intent = new Intent(activity, MainActivity.class);
+                                        activity.startActivity(intent);
+                                        activity.finish();
+                                    } else {
+                                        Log.d("login", e.getMessage()+e);
+                                        Toast.makeText(getActivity(), e.getMessage() ,Toast.LENGTH_LONG).show();
+                                        // Signup failed. Look at the ParseException to see what happened.
+                                    }
+                                }
+                            });
+
+                            Log.d("score", "Retrieved the object.");
+                        }
+                    }
+                });
+
+
             }
         });
 
@@ -109,7 +146,18 @@ public class SignUpFragment extends Fragment {
                     currentUser.save();
                 } else {
                     currentUser.setPassword("ChestreamPasswordYo");
-                    currentUser.signUp();
+                    currentUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e==null)
+                            {
+                                Toast.makeText(getActivity(), "Welcome !",Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(getActivity(), "Please choose another username and try again !",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
 //                    currentUser.saveInBackground();
                 }
             } catch (ParseException e) {
